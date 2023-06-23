5ba855b7c06d38e046a3279d8d6ab881a2c75765
Moonsolos <1056860@hr.nl>
2023-06-23 13:21:28 +0200

    Fixed problem with clouds

--- src/js/Wolken.js
+++ src/js/Wolken.js
@@ -14,31 +14,31 @@ export class Wolken extends Actor {
     offset
 
     onInitialize(engine){
-        const spaceImage2 = Resources.wolken.toSprite()
-        this.offset = spaceImage2.height
+        const spaceImage3 = Resources.wolken.toSprite()
+        this.offset = spaceImage3.height
 
-        const group2 = new GraphicsGroup({
+        const group3 = new GraphicsGroup({
             members: [
                 {
-                    graphic: spaceImage2,
+                    graphic: spaceImage3,
                     pos: new Vector(0, 0),
                 },
                 {
-                    graphic: spaceImage2,
-                    pos: new Vector(0, spaceImage2.height),
+                    graphic: spaceImage3,
+                    pos: new Vector(0, spaceImage3.height),
                 }
             ]
         })
 
         this.graphics.anchor = new Vector(0,0)
-        this.graphics.add(group2)
+        this.graphics.add(group3)
         this.pos = new Vector(0, 0)
         this.scale = new Vector(4,4)
         this.vel = new Vector(0, -50)
     }
 
     onPostUpdate(engine, delta) {
-        if (this.pos.y < -this.offset) {
+        if (this.pos.y > this.offset) {
             this.pos = new Vector(0, 0)
         }
     }
